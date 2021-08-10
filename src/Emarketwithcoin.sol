b91e8df660858cca2f4a725c7d7db6da3fdd77fa
drigo <rodrigo.princi@gmail.com>
2021-08-10 15:28:32 +0200

    init

--- src/Emarketwithcoin.sol
+++ src/Emarketwithcoin.sol
@@ -1,4 +1,67 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
+import "ds-token/token.sol";
+
 contract Emarketwithcoin {
+	struct Item {
+	    string description;
+	    address seller;
+	    address buyer;
+	    uint price;
+	    bool sold;
+	}
+
+	ERC20 public emark;
+
+	mapping(uint256=>Item) public items;
+	uint256 public itemCount;
+
+	constructor (address _emark) public {
+		emark = ERC20(_emark);
+	}
+
+	function addItem(string memory _description, uint _price) public returns(uint) {
+	    itemCount++;
+	    items[itemCount].description = _description;
+	    items[itemCount].seller = msg.sender;
+	    items[itemCount].price = _price;
+	    return itemCount;
+	}
+
+	function getItem(uint _index) public view returns(string memory, uint){
+	    Item storage i = items[_index];
+	    require(i.seller != address(0), "no such item"); // not exists
+	    return(i.description, i.price);
+	}
+
+	function checkItemExisting(uint _index) public view returns (bool) {
+	    Item storage i = items[_index];
+	    return (i.seller != address(0));
+	}
+
+	function checkItemSold(uint _index) public view returns (bool) {
+	    Item storage i = items[_index];
+	    require(i.seller != address(0), "no such item"); // not exists
+	    return i.sold;
+	}
+
+	function removeItem(uint _index) public {
+	    Item storage i = items[_index];
+	    require(i.seller != address(0), "no such item"); // not exists
+	    require(i.seller == msg.sender, "only seller can remove item");
+	    require(!i.sold, "item sold already");
+	    delete items[_index];
+	}
+
+	function buyItem(uint _index) public {
+	    Item storage i = items[_index];
+	    require(i.seller != address(0), "no such item"); // not exists
+	    require(!i.sold, "item sold already");
+	    require(i.price <= emark.balanceOf(msg.sender), "not enough tokens");
+	    i.buyer = msg.sender;
+	    i.sold = true;
+	    emark.transferFrom(msg.sender, i.seller, i.price);
+	}
+
 }
